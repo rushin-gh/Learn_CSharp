00d8e7f84ee01e0ff9d1766f09fa7667e4c182ef
Rushi <rushin.mailme@gmail.com>
2025-01-20 21:48:57 +0530

    Main method

---
+++ Main_Method/Program.cs
@@ -0,0 +1,9 @@
+﻿// Main Method - An entry point of an application
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Application started executing from here...");
+    }
+}
