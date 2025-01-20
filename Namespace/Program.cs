2a532dbe96fe3f600027bb130be2271110f861a0
Rushi <rushin.mailme@gmail.com>
2025-01-20 21:51:09 +0530

    Namespace

---
+++ Namespace/Program.cs
@@ -0,0 +1,11 @@
+﻿// Namespace - Logical origanisation of code
+
+namespace Namespace {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
